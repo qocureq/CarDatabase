4cdeba67c7338236d3ef5a924e6b58027bbd4ed9
Maks <makskocur@gmail.com>
2022-11-07 22:07:17 +0100

    Initial Commit
    Create a simple CMake project

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * 1. Przechowywanie danych aut o strukturze: Marka, Model, Rok Produkcji, Generacja, (op)Wersja, Kolor, Silnik(pojemność/nazwa), Moc, Imię/nazwisko właściciela, tablica rejestracyjna
+ * 2. Dodwanie nowych aut
+ * 3. Wyświetlenie całej bazy danych
+ * 4. Wyszukiwanie po marce/modelu/właścicielu/kolorze
+ * 5. Wyszukianie po najwyższej mocy
+ * 6. Sortowanie po  marce/modelu
+ */
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
